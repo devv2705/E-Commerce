402c3cb3dad924073df6166394dc7861146fb838
p.devv <pd27052003@gmail.com>
2024-03-03 02:44:14 +0530

    signin with jwt token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,8 @@
  */
 const bcrypt = require('bcryptjs')
 const user_model=require("../models/user.model")
+const jwt = require('jsonwebtoken')
+const secret=require('../configs/auth.config')
 
 exports.signup = async(req,res)=>{
     /** logic to create user */
@@ -48,4 +50,47 @@ exports.signup = async(req,res)=>{
         })
         //500 indicates server error
     }
+}
+
+exports.signin=async (req,res)=>{
+
+    //check user id is present in the database
+
+    const user=await user_model.findOne({userId:req.body.userId})
+
+    if(user==null){
+       return res.status(400).send({
+            message:"userId is not valid!!!!"
+        })
+    }
+
+    //if password is correct
+    //check password that user provide and pass in database 
+    //but here user pass is incrypted and req has pass in string
+    const isPasswordValid=bcrypt.compareSync(req.body.password,user.password)
+
+    if(!isPasswordValid){
+       return res.status(401).send({
+            message:"wrong password passed"
+        })
+    }
+
+    //using jwt -> create access tocken with given ttl(time to leave) and return
+
+
+    //to create jwt token we have to pass on what basis we want to creat 
+    //also provide one secret string rendom and expiry time
+    //this secret we define in config file 
+    const token=jwt.sign({id:user.userId},secret.secret,{
+        expiresIn:120
+    })
+
+    res.status(200).send({
+        name:user.name,
+        userId:user.userId,
+        email:user.email,
+        userType:user.userType,
+        accessToken:token
+    })
+
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,22 +1,31 @@
+
+const authController = require("../controllers/auth.controller")
+
+const authMw = require("../middlewares/auth_mw")
+
+module.exports = (app) => {
 /**
  * POST : localhost:8888/ecomm/api/v1/auth/signup
  * 
  * need to intercept this
  */
 
-const authController=require("../controllers/auth.controller")
-
-const authMw=require("../middlewares/auth_mw")
-
-module.exports=(app)=>{
     //if post call comes from above uri then give the controll to the signup 
 
-    app.post("/ecomm/api/v1/auth/signup",[authMw.verifySignUpBody],authController.signup)
+    app.post("/ecomm/api/v1/auth/signup", [authMw.verifySignUpBody], authController.signup)
 
     //here i have appied middle ware for req varify in [].
     //once it varify through mw then it pass for registration
 
-    
+
+
+    /**
+     * crete route for sign in
+     * POST : localhost:8888/ecomm/api/v1/auth/signin
+     */
+    app.post("/ecomm/api/v1/auth/signin",authController.signin)
+
+
 
 }
 
