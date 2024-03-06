071f2e68b0551696631f20182b6369d925d2c3f4
p.devv <pd27052003@gmail.com>
2024-03-06 22:12:14 +0530

    cart added

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,5 +1,6 @@
 const cart_model = require("../models/cart.model");
 const category_model = require("../models/category.model");
+const { product } = require("../models/product.model");
 
 exports.addToCart = async (req, res) => {
     try {
@@ -54,5 +55,58 @@ exports.addToCart = async (req, res) => {
         });
     }
 };
+exports.delFromCart=async(req,res)=>{
 
+        try{
+
+            const productName=req.body.name
+            // const category=await category_model.findOne({"product.name":productName})
+            // const product=await category.findOne(product=>product.name==productName)
+            const product=await cart_model.findOne({name:productName})
+            const quantity=await product.quantity
+            if(quantity!=1){
+                product.quantity-=1
+                product.save()
+                return res.status(201).send({
+                    message:`1 quantity of ${productName} deleted`
+                })
+            }
+            else{
+                const deletee=await cart_model.deleteOne(product)
+                return res.status(201).send({
+                    message:`remove the ${productName} from cart`
+                })
+            }
+
+        }catch(err){
+            console.log("error occured while deleting from cart")
+            return res.status(501).send({
+                message:"Error while deleteing froduct from cart"
+            })
+        }
+}
+
+exports.readCart=async (req,res)=>{
+    //if api hit through name then it will return that name product detail
+    //but if req.body is empty then return whole cart of user
+    try{
+
+        const productName=req.body.name
+        const cart=await cart_model.find()
+
+        if(!productName){
+            return res.status(201).send(cart)
+        }
+        else{
+        const data=await cart_model.findOne({name:productName})
+        return res.status(201).send(data)
+        }
+
+    }catch(err){
+        console.log("error while fetching cart data")
+        return res.status(500).send({
+            message:"error occured while fetching the cart detail"
+        })
+    }
+}
 
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -9,4 +9,16 @@ module.exports=(app)=>{
      */
     app.post("/ecomm/api/v1/addtocart",[auth_mw.verify_Token,auth_mw.isAdmin],cartController.addToCart)
 
+    /**
+     * delete from cartproduct
+     * POST :localhost:8888/ecomm/api/v1/deletecart
+     */
+    app.post("/ecomm/api/v1/deletecart",[auth_mw.verify_Token,auth_mw.isAdmin],cartController.delFromCart)
+
+
+    /**
+     * delete from cartproduct
+     * POST :localhost:8888/ecomm/api/v1/readcart
+     */
+    app.get("/ecomm/api/v1/readcart",[auth_mw.verify_Token,auth_mw.isAdmin],cartController.readCart)
 }
